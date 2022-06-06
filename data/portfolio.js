bf61535d8c4772b2f0292201c52f32c4d0b3cd5a
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-06 09:40:45 -0600

    adds pages objects

--- data/portfolio.js
+++ data/portfolio.js
@@ -8,7 +8,36 @@ const workPieces = [
   },
 ]
 
+const pages = [
+  {
+    navText: "About",
+    title: "About",
+    id: "about-section",
+    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure laboriosam minus qui unde sapiente ea commodi voluptatum a, delectus, rerum quis labore illum sunt dolorum accusamus architecto, dignissimos esse porro.
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam sit nostrum porro odio, pariatur delectus ex quaerat nam explicabo magni illum praesentium quis quam voluptatum voluptatem earum ab. Perferendis, obcaecati. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis commodi minima, nisi esse impedit fuga doloremque aperiam repellat deserunt nulla soluta qui ut recusandae sunt debitis obcaecati quia. Doloribus, perspiciatis.`,
+  },
+  {
+    navText: "Resume",
+    title: "Resume",
+    id: "resume-section",
+    content: "",
+  },
+  {
+    navText: "Contact",
+    title: "Contact Points",
+    id: "contact-section",
+    content: "",
+  },
+  {
+    navText: "Work",
+    title: "Work",
+    id: "work-section",
+    content: "",
+  },
+]
+
 
 export {
-  workPieces
+  workPieces,
+  pages
 }
